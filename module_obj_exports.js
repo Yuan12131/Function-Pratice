08ac1a0688d7be28ca12b7a011a6d84474bf8212
leeyuan <dbdks32123@gmail.com>
2023-09-29 16:38:00 +0900

    사칙연산  함수 객체  module화 내보내기

---
+++ module_obj_exports.js
@@ -0,0 +1,19 @@
+const sachick = {
+  add : function add (first, second){
+    return first + second
+  },
+
+  minus : function minus (first, second){
+    return first - second
+  },
+
+  multiply : function multiply(first, second){
+    return first * second
+  },
+
+  dividion : function division(first, second){
+    return first / second
+  }
+}
+
+module.exports = sachick
